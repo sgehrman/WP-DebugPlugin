96be2f13c31f2da63a718b685acf827b69ac9b45
Steve Gehrman <sgehrman@gmail.com>
2016-02-07 03:28:47 -0800

    fixes

--- debugplugin.php
+++ debugplugin.php
@@ -1,4 +1,4 @@
-<?php
+<?
 /*
 Plugin Name: WP Debug Plugin
 Version: v1.0
@@ -10,7 +10,6 @@ Description: Random Debug stuff
 
 define('CREATION_SECRET_KEY', 'www');
 define('YOUR_VERIFICATION_SECRET_KEY', '56b458f3613364.08018088');
-define('YOUR_LICENSE_SERVER_URL', 'http://127.0.0.1/cocoatech');
 
 add_action('admin_menu', 'slm_sample_license_menu');
 
@@ -30,14 +29,32 @@ function license_management_page()
     echo '<div class="wrap">';
     echo '<h2>License Manager</h2>';
 
-    $lic = new LicenseManager(YOUR_LICENSE_SERVER_URL, YOUR_VERIFICATION_SECRET_KEY, CREATION_SECRET_KEY);
+    $site_url = network_site_url('/');
 
+    $lic = new LicenseManager($site_url, YOUR_VERIFICATION_SECRET_KEY, CREATION_SECRET_KEY);
+
+    handleButtonClicks($lic);
+
+    if ($lic->is_licensed()) {
+        echo deactivateForm();
+    } else {
+        echo activateForm();
+    }
+
+    echo createForm();
+    dumpShit();
+
+    echo '</div>';
+}
+
+function handleButtonClicks($lic)
+{
     // License activate button was clicked
     if (isset($_REQUEST['activate_license'])) {
         $licenseKey = $_REQUEST['input_license_key'];
 
         if ($lic->active($licenseKey)) {
-            echo 'You license Activated successfuly';
+            echo 'You license Activated successfully';
         } else {
             echo $lic->err;
         }
@@ -46,53 +63,40 @@ function license_management_page()
         $licenseKey = $_REQUEST['input_license_key'];
 
         if ($lic->deactivate($licenseKey)) {
-            echo 'You license Deactivated successfuly';
+            echo 'You license Deactivated successfully';
+        } else {
+            echo $lic->err;
+        }
+    } else if (isset($_REQUEST['create_license'])) {
+        // License deactivate button was clicked
+        $firstName   = $_REQUEST['input_first_name'];
+        $lastName    = $_REQUEST['input_last_name'];
+        $email       = $_REQUEST['input_email'];
+        $company     = $_REQUEST['input_company'];
+        $numInstalls = $_REQUEST['input_installs'];
+
+        if ($lic->create($firstName, $lastName, $email, $company, $numInstalls)) {
+            echo 'You License creasted successfully';
         } else {
             echo $lic->err;
         }
     }
-
-    if ($lic->is_licensed()) {
-        echo 'Thank You for Purchasing!';
-
-        ?>
-        <form action="" method="post">
-            <table class="form-table">
-                <tr>
-                    <th style="width:100px;"><label for="input_license_key">License Key</label></th>
-                    <td ><input class="regular-text" type="text" id="input_license_key" name="input_license_key"  value="<?echo get_option(license_option_key()); ?>" ></td>
-                </tr>
-            </table>
-            <p class="submit">
-                <input type="submit" name="deactivate_license" value="Deactivate" class="button-primary" />
-            </p>
-        </form>
-        <?
-    } else {
-        ?>
-        <form action="" method="post">
-            <table class="form-table">
-                <tr>
-                    <th style="width:100px;"><label for="input_license_key">License Key</label></th>
-                    <td ><input class="regular-text" type="text" id="input_license_key" name="input_license_key"  value="<?echo get_option(license_option_key()); ?>" ></td>
-                </tr>
-            </table>
-            <p class="submit">
-                <input type="submit" name="activate_license" value="Activate" class="button-primary" />
-            </p>
-        </form>
-        <?
-    }
-
-    echo '</div>';
 }
 
 function logg($data)
 {
     if (is_array($data)) {
-        $output = "<script>alert( 'Logg: " . implode(',', $data) . "' );</script>";
+        $output = "<script>alert( 'Loga: " . implode(',', $data) . "' );</script>";
+    } else if (is_object($data)) {
+        $duh = 'OK: ';
+        foreach ($data as $key => $value) {
+            $duh = $duh . "$key => $value, ";
+        }
+        $output = "<script>alert( 'Logo: " . $duh . "' );</script>";
+    } else if (is_string($data)) {
+        $output = "<script>alert( 'Logs: " . $data . "' );</script>";
     } else {
-        $output = "<script>alert( 'Logg: " . $data . "' );</script>";
+        $output = "<script>alert( 'Log?: " . gettype($data) . "' );</script>";
     }
 
     echo $output;
@@ -145,25 +149,24 @@ class LicenseManager
             return true;
         } else {
             $this->err = $license_data->message;
-
         }
 
         return false;
     }
 
-    public function create()
+    public function create($firstName, $lastName, $email, $company, $numInstalls)
     {
         $api_params = array(
             'slm_action'          => 'slm_create_new',
             'secret_key'          => $this->creation_secret_key,
-            'first_name'          => 'elvis',
-            'last_name'           => 'presley',
-            'email'               => 'elvis@king.com',
-            'company_name'        => 'XYZ',
+            'first_name'          => $firstName,
+            'last_name'           => $lastName,
+            'email'               => $email,
+            'company_name'        => $company,
             'txn_id'              => 'ABC0987654321',
-            'max_allowed_domains' => '10',
-            'date_created'        => date('Y - m - d'),
-            'date_expiry'         => '2017 - 01 - 01',
+            'max_allowed_domains' => numInstalls,
+            'date_created'        => date('Y-m-d'),
+            'date_expiry'         => '',
         );
 
         $query    = add_query_arg($api_params, $this->server);
@@ -173,13 +176,13 @@ class LicenseManager
             echo "Unexpected Error! The query returned with an error.";
         } else {
             $license_data = json_decode(wp_remote_retrieve_body($response));
-        }
 
-        if ($license_data->result == 'success') {
-
-            return true;
-        } else {
-            $this->err = $license_data->message;
+            if ($license_data->result == 'success') {
+                update_option(license_option_key(), $license_data->key);
+                return true;
+            } else {
+                $this->err = $license_data->message;
+            }
         }
 
         return false;
@@ -195,7 +198,7 @@ class LicenseManager
             'item_reference='    => urlencode($this->product_id),
         );
 
-        $query    = add_query_arg($api_params, $this->server);
+        $query = add_query_arg($api_params, $this->server);
 
         $response = wp_remote_get($query, array('timeout' => 20, 'sslverify' => false));
 
@@ -246,4 +249,97 @@ class LicenseManager
 
 }
 
-?>
+// -------------------------------------------------------------------------------
+// html
+
+function activateForm()
+{
+    $licenseKey = get_option(license_option_key());
+
+    $html = <<<HTML
+
+   <form action="" method="post">
+            <table class="form-table">
+                <tr>
+                    <th style="width:100px;"><label for="input_license_key">License Key</label></th>
+                    <td ><input class="regular-text" type="text" id="input_license_key" name="input_license_key"  value=$licenseKey ></td>
+                </tr>
+            </table>
+            <p class="submit">
+                <input type="submit" name="activate_license" value="Activate" class="button-primary" />
+            </p>
+        </form>
+HTML;
+    return $html;
+}
+
+function deactivateForm()
+{
+    $licenseKey = get_option(license_option_key());
+
+    $html = <<<HTML
+
+  <form action="" method="post">
+            <table class="form-table">
+                <tr>
+                    <th style="width:100px;"><label for="input_license_key">License Key</label></th>
+                    <td ><input class="regular-text" type="text" id="input_license_key" name="input_license_key"  value=$licenseKey ></td>
+                </tr>
+            </table>
+            <p class="submit">
+                <input type="submit" name="deactivate_license" value="Deactivate" class="button-primary" />
+            </p>
+        </form>
+HTML;
+    return $html;
+}
+
+function createForm()
+{
+    $firstName = 'Xxook';
+    $lastName  = 'Quuook';
+    $email     = 'Xxook@aol.com';
+
+    $html = <<<HTML
+
+  <form action="" method="post">
+            <table class="form-table">
+                <tr>
+                    <th style="width:100px;"><label for="input_first_name">First Name</label></th>
+                    <td ><input class="regular-text" type="text" id="input_first_name" name="input_first_name"  value=$firstName ></td>
+                </tr>
+                <tr>
+                    <th style="width:100px;"><label for="input_last_name">Last Name</label></th>
+                    <td ><input class="regular-text" type="text" id="input_last_name" name="input_last_name"  value=$lastName ></td>
+                </tr>
+                <tr>
+                    <th style="width:100px;"><label for="input_email">Email</label></th>
+                    <td ><input class="regular-text" type="text" id="input_email" name="input_email"  value=$email ></td>
+                </tr>
+                <tr>
+                    <th style="width:100px;"><label for="input_company">Company</label></th>
+                    <td ><input class="regular-text" type="text" id="input_company" name="input_company"  value='' ></td>
+                </tr>
+                <tr>
+                    <th style="width:100px;"><label for="input_installs">Install Count</label></th>
+                    <td ><input class="regular-text" type="number" id="input_installs" name="input_installs"  value='4' ></td>
+                </tr>
+            </table>
+            <p class="submit">
+                <input type="submit" name="create_license" value="Create License" class="button-primary" />
+            </p>
+        </form>
+HTML;
+    return $html;
+}
+
+function dumpShit()
+{
+    $site_title       = get_bloginfo('name');
+    $site_url         = network_site_url('/');
+    $site_description = get_bloginfo('description');
+
+    echo 'The Network Home URL is: ' . $site_url;
+    echo 'The Network Home Name is: ' . $site_title;
+    echo 'The Network Home Tagline is: ' . $site_description;
+}
